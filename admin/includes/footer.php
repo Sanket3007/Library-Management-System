776c630b6cbfc5fd30fa3b8c98495847d4ec85c7
Sanket3007 <karanjesanket1980@gmail.com>
2021-07-07 13:29:56 +0530

    Final

---
+++ admin/includes/footer.php
@@ -0,0 +1,6 @@
+<footer class="main-footer">
+    <div class="pull-right hidden-xs">
+     
+    </div>
+    <strong>&copy; 2021 - Library Management System | Brought To You By <a href="https://code-projects.org/">Avadesh Singh</a></strong>
+</footer>
---
+++ includes/footer.php
@@ -0,0 +1,9 @@
+<footer class="main-footer">
+    <div class="container">
+      <div class="pull-right hidden-xs">
+        <b>Open Admin Panel and Add/View user to know Login ID</b>
+      </div>
+      <strong>&copy; 2021 - Library Management System | Brought To You By <a href="https://code-projects.org/">Avadesh Singh</a></strong>
+      </div>
+    <!-- /.container -->
+</footer>
